431465689312fcac74f651dd3f188c909f914793
Victor Monterroso <vicmmonterroso@gmail.com>
2018-05-22 03:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-  name.push("kitten");
-  return 
+  kittens.push("kitten");
 }
